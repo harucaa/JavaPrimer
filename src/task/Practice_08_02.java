79122ecf672a800549085522264cab1d0f7c242d
naohiro2g <587586+Naohiro2g@users.noreply.github.com>
2022-11-28 23:24:46 +0900

    use Treemap instead of HashMap

--- src/task/Practice_08_02.java
+++ src/task/Practice_08_02.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 // 練習問題#8  Practice_08_02.java
 
@@ -21,15 +22,18 @@ import java.util.Map;
 // 
 // faculty, department, bukatsu, homeroom, studentId
 
+// HashMapだとソートが謎の動き。数値によってダメな時がある。TreeMapが良さげ。
+
 public class Practice_08_02 {
 
 	public static void main(String[] args) {
 
-		HashMap<Integer, UnivStudent> students = new HashMap<>();
+		//		HashMap<Integer, UnivStudent> students = new HashMap<>();
+		Map<Integer, UnivStudent> students = new TreeMap<>();
 
-		students.put(20220904, new UnivStudent("Naohiro Tsuji", "工学部", "電子工学科", 2));
+		students.put(20220804, new UnivStudent("Naohiro Tsuji", "工学部", "電子工学科", 2));
 		students.put(20220105, new UnivStudent("Miku Hatsune", "情報学部", "行動情報学科", 1));
-		students.put(20220109, new UnivStudent("Akito Shinonome", "情報学部", "情報科学学科", 3));
+		students.put(20220103, new UnivStudent("Akito Shinonome", "情報学部", "情報科学学科", 3));
 		students.put(20220203, new UnivStudent("Honami Mochizuki", "情報学部", "情報社会学科", 4));
 
 		System.out.println("ID指定でアクセスして表示");
@@ -41,7 +45,7 @@ public class Practice_08_02 {
 				+ " : " + students.get(studentId).getGrade());
 		System.out.println();
 
-		System.out.println("イテレーターで取り出す。"); // 次のソートと同じ動作。おかしい結果。
+		System.out.println("イテレーターで取り出す。"); // すでにソート済み
 		Iterator<Map.Entry<Integer, UnivStudent>> itr = students.entrySet().iterator();
 		while (itr.hasNext()) {
 			Map.Entry<Integer, UnivStudent> student = itr.next();
@@ -54,9 +58,9 @@ public class Practice_08_02 {
 		System.out.println();
 
 		// keyでソートしたい。以下、テスト。。
-		// keyの値によって、ソートが成功するときとダメな時がある。
+		// keyの値によって、ソートが成功するときとダメな時がある。TreeMapだとソート不要。
 
-		System.out.println("キーでソートして昇順で表示"); // おかしい。
+		System.out.println("キーでソートして昇順で表示"); //
 		Object[] mapkey = students.keySet().toArray();
 		Arrays.sort(mapkey);
 		for (Integer nKey : students.keySet()) {
@@ -64,22 +68,22 @@ public class Practice_08_02 {
 		}
 		System.out.println();
 
-		//  お試し中　おかしい orangeのキーによってダメな時有り。
-		Map<Integer, String> mMap = new HashMap<Integer, String>();
+		//  お試し中
+		Map<Integer, String> mMap = new TreeMap<Integer, String>();
 		// Mapにデータを格納
 		mMap.put(2, "apple");
-		mMap.put(19, "orange");
+		mMap.put(99, "orange");
 		mMap.put(3, "pineapple");
 		//		mMap.put(3, "strawberry");
 		//		mMap.put(5, "melon");
 
-		// キーでソートする なんかおかしい
+		// キーでソートする
 		Object[] mapkey2 = mMap.keySet().toArray();
 		Arrays.sort(mapkey2);
-
 		for (Integer nKey : mMap.keySet()) {
 			System.out.println(nKey + " " + mMap.get(nKey));
 		}
+		System.out.println();
 
 		// Listでソートの方法 Stringのソート　これは正しい
 		Map<String, String> map = new HashMap<>();
@@ -93,13 +97,14 @@ public class Practice_08_02 {
 		for (String key : keyList) {
 			System.out.println("key: " + key + ", value: " + map.get(key));
 		}
+		System.out.println();
 
-		// Listでソートの方法 Integerのソート なんかおかしい
-		Map<Integer, String> map2 = new HashMap<>();
+		// Listでソートの方法 Integerのソート
+		Map<Integer, String> map2 = new TreeMap<>();
 		map2.put(1, "Kathmandu");
 		map2.put(4, "Washington");
 		map2.put(3, "New Delhi");
-		map2.put(22, "London");
+		map2.put(99, "London");
 		map2.put(5, "Canberra");
 		List<Integer> keyList2 = new ArrayList<>(map2.keySet());
 		keyList.sort((s1, s2) -> s1.compareTo(s2));
