d10d52f6c14cafe07d25cc2add5f3381709d0c7b
naohiro2g <587586+Naohiro2g@users.noreply.github.com>
2022-12-06 14:49:05 +0900

    Practice 9, 10  & Task 4, 5

---
+++ src/task/Practice_10_01.java
@@ -0,0 +1,27 @@
+package task;
+
+//# 練習問題#10 
+
+//
+//## 問題（１）
+//  ReviewSample10.java
+//  楽天市場レビューのサンプルデータについて、
+//  レビュー１つ分を表すのに適切なクラスを作成せよ。
+//
+//  Practice_10_01.java
+//  メインメソッドでデータ(TSV形式)の全てを読み込み、全データを適切なListあるいはMapに収納せよ。
+
+public class Practice_10_01 {
+	public static void main(String[] args) {
+
+	}
+}
+
+//## 補足
+//
+//区切り文字(TSVなら\t)で区切られた文字列を分割するときには、String#splitを使う。
+//
+//正規表現を使う方法なんかもある。例によって自分で考えて好きにやろう。
+//
+//Stringからintに変換するときは、キャストでなくInteger#parseIntを使う。
+//(doubleに変換するときも似たような感じ)
